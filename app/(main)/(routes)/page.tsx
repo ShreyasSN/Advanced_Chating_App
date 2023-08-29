f33a215deff28bcf0fb4343c4022ce3403392d0d
Shreyas S N <shreyassn91224@gmail.com>
2023-08-29 22:22:07 +0530

    Add

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,8 +1,14 @@
+import { ModeToggle } from "@/components/mode-toggle"
+import { UserButton } from "@clerk/nextjs/app-beta"
 
 export default function Home() {
   return (
    <div>
-     Protected Page
+      <UserButton
+          afterSignOutUrl="/"
+      />
+    <ModeToggle/>
+      
     </div>
   )
 }
